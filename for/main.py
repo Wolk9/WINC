9b248080d8db6e0aecaffcc67bfb43281cd25de7
Martin de Bes <wolk9@me.com>
2023-05-20 21:36:24 +0200

    for

--- for/main.py
+++ for/main.py
@@ -9,6 +9,17 @@ __human_name__ = "for"
 
 """ Write your functions here. """
 
+def shortest_names(countries):
+    return
+
+def most_vowels(countries):
+    return
+
+def alphabet_set(countries):
+    return
+
+
+
 
 # This block is only run if this file is the entrypoint; python main.py
 # It is not run if it is imported as a module: `from main import *`
