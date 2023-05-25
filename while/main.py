662d718b167f89c3efbfc318fd365a966d622af8
Martin de Bes <wolk9@me.com>
2023-05-25 18:13:06 +0200

    unique_koala_facts in progress

--- while/main.py
+++ while/main.py
@@ -7,17 +7,20 @@ __human_name__ = "while"
 
 def unique_koala_facts(number):
     unique_koala_facts = []
+    unique_koala_facts_numbered = []
     
     while number > 0:
         koala_fact = random_koala_fact();    
         if koala_fact not in unique_koala_facts:
             print(koala_fact + " staat er nog niet in!")
             unique_koala_facts.append(koala_fact)
-            time.sleep(1)
+            
+            #time.sleep(1)
         else:
             index = unique_koala_facts.index(koala_fact)
-            time.sleep(0.5)
+            #time.sleep(0.5)
             print(koala_fact + "\nstaat er in op index: " + str(index) )
+            
         number += -1
         print("\nnumber: ", number)
         print("list: ", unique_koala_facts, "\n\n")
