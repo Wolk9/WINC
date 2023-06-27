00a44a4ce53f2adbc67fec88081dc011041fd569
Martin de Bes <wolk9@me.com>
2023-06-27 13:06:28 +0200

    while succeded and cleaned up with flake8

--- while/main.py
+++ while/main.py
@@ -1,5 +1,4 @@
 from helpers import random_koala_fact
-import time 
 
 __winc_id__ = "c0dc6e00dfac46aab88296601c32669f"
 __human_name__ = "while"
@@ -11,11 +10,11 @@ def unique_koala_facts(number):
     unique_koala_facts = []
     iteration_limit = 1000
     iterations = 0
-    
+
     while number > 0 and iterations < iteration_limit:
         koala_fact = random_koala_fact()
         iterations += 1
-        
+
         if koala_fact not in unique_koala_facts:
             unique_koala_facts.append(koala_fact)
             number -= 1
@@ -46,27 +45,26 @@ def num_joey_facts():
     return len(unique_joey_facts)
 
 
-
-
 def koala_weight():
     weight_fact = random_koala_fact()
-    
+
     # Zoek naar het feit over het gewicht in de vorm van "Het gewicht van een koala is X pond."
     # Pas deze code aan als de indeling van het feit varieert.
     while "pounds" not in weight_fact:
         weight_fact = random_koala_fact()
-    
+
     # Haal de waarde van het gewicht uit het feit
     weight_in_pounds = int(weight_fact.split(" ")[-2])
-    
+
     # Converteer ponden naar kilogram (1 pond = 0.453592 kg)
     weight_in_kg = round(weight_in_pounds * 0.453592)
-    
+
     return weight_in_kg
-    
+
 # This block is only executed if this script is run directly (python main.py)
 # It is not run if you import this file as a module.
+
+
 if __name__ == "__main__":
     unique_koala_facts()
     koala_weight()
-    
