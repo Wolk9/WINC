0fc33f90f7e62801160c5bee775f0bf0b641af6e
Martin de Bes <wolk9@me.com>
2023-06-27 16:22:16 +0200

    arguments part2

--- arguments/main.py
+++ arguments/main.py
@@ -11,18 +11,19 @@ def greet(name, greeting_template='Hello, <name>!'):
 
 def force(mass, body='earth'):
     gravity_factors = {
-        'mercury': 3.7,
-        'venus': 8.87,
-        'earth': 9.8,
-        'mars': 3.71,
-        'jupiter': 24.79,
-        'saturn': 10.44,
-        'uranus': 8.69,
-        'neptune': 11.15,
-        'pluto': 0.62,
-        'moon': 1.62
+        "sun": 274,
+        "jupiter": 24.92,
+        "neptune": 11.15,
+        "saturn": 10.44,
+        "earth": 9.798,
+        "uranus": 8.87,
+        "venus": 8.87,
+        "mars": 3.71,
+        "mercury": 3.7,
+        "moon": 1.62,
+        "pluto": 0.58,
     }
-    
+
     if body.lower() in gravity_factors:
         gravity = round(gravity_factors[body.lower()], 1)
     else:
