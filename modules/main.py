472bcbce3bac60da6c26b20db21d0488bda8f122
Martin de Bes <wolk9@me.com>
2023-06-27 17:08:13 +0200

    moles step 3 and 4

--- modules/main.py
+++ modules/main.py
@@ -5,8 +5,20 @@ __human_name__ = 'modules'
 # Add your code after this line
 import this
 import time
+import math
+from datetime import datetime
+
 
 
 def wait(seconds):
     time.sleep(seconds)
 
+
+def my_sin(number):
+    return math.sin(number)
+
+
+def iso_now():
+    now = datetime.now()
+    iso_format = now.strftime("%Y-%m-%dT%H:%M")
+    return iso_format
