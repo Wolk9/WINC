3abff1a2caba26e07fa09f307a263c11f501aa75
Martin de Bes <wolk9@me.com>
2023-07-01 21:27:32 +0200

    superpy

--- superpy/main.py
+++ superpy/main.py
@@ -1,7 +1,8 @@
 # Imports
 import argparse
 import csv
-from datetime import date
+import datetime
+import os
 
 # Do not change these lines.
 __winc_id__ = "a2bc36ea784242e4989deb157d527ba0"
@@ -9,9 +10,224 @@ __human_name__ = "superpy"
 
 
 # Your code below this line.
+
+DATA_DIR = os.path.join(os.getcwd(), "data")
+TODAY_FILE = os.path.join(DATA_DIR, "today.txt")
+BOUGHT_FILE = os.path.join(DATA_DIR, "bought.csv")
+SOLD_FILE = os.path.join(DATA_DIR, "sold.csv")
+
+
+def create_data_dir():
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+
+
+def set_today(date):
+    with open(TODAY_FILE, "w") as file:
+        file.write(date.strftime("%Y-%m-%d"))
+    print("OK")
+
+
+def get_today():
+    with open(TODAY_FILE, "r") as file:
+        today_str = file.read().strip()
+    return datetime.datetime.strptime(today_str, "%Y-%m-%d").date()
+
+
+def advance_time(days):
+    today = get_today()
+    new_date = today + datetime.timedelta(days=days)
+    set_today(new_date)
+
+
+def buy_product(product_name, price, expiration_date):
+    bought_id = get_next_id(BOUGHT_FILE)
+    row = [bought_id, product_name, get_today().strftime("%Y-%m-%d"), price, expiration_date]
+    with open(BOUGHT_FILE, "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
+    print("OK")
+
+
+def sell_product(product_name, price):
+    bought_id = find_product_in_stock(product_name)
+    if bought_id:
+        sold_id = get_next_id(SOLD_FILE)
+        row = [sold_id, bought_id, get_today().strftime("%Y-%m-%d"), price]
+        with open(SOLD_FILE, "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(row)
+        print("OK")
+    else:
+        print("ERROR: Product not in stock.")
+
+
+def get_next_id(file_path):
+    with open(file_path, "r") as file:
+        reader = csv.reader(file)
+        header = next(reader, None)  # Skip the header row
+        if header is None:
+            return 1  # Return 1 as the default ID if there are no rows
+        next_id = max(int(row[0]) for row in reader) + 1
+    return next_id
+
+
+
+def find_product_in_stock(product_name):
+    with open(BOUGHT_FILE, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[1] == product_name and not is_product_sold(row[0]):
+                return row[0]
+    return None
+
+
+def is_product_sold(bought_id):
+    with open(SOLD_FILE, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[1] == bought_id:
+                return True
+    return False
+
+
+def get_inventory_report():
+    rows = []
+    with open(BOUGHT_FILE, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if not is_product_sold(row[0]):
+                rows.append(row[1:])
+    return rows
+
+
+def get_revenue_report(date):
+    revenue = 0
+    with open(SOLD_FILE, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            sell_date = datetime.datetime.strptime(row[2], "%Y-%m-%d").date()
+            if sell_date == date:
+                revenue += float(row[3])
+    return revenue
+
+
+def get_profit_report(date):
+    revenue = 0
+    cost = 0
+    with open(SOLD_FILE, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            sell_date = datetime.datetime.strptime(row[2], "%Y-%m-%d").date()
+            if sell_date == date:
+                revenue += float(row[3])
+                bought_id = row[1]
+                cost += get_buy_price(bought_id)
+    return revenue - cost
+
+
+def get_buy_price(bought_id):
+    with open(BOUGHT_FILE, "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == bought_id:
+                return float(row[3])
+    return 0.0
+
+
+def export_csv(file_path, rows):
+    with open(file_path, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+    print(f"Data exported to {file_path}")
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(prog="SuperPy", description="Supermarket Inventory Management")
+    subparsers = parser.add_subparsers(dest="command", title="commands")
+
+    parser_set_today = subparsers.add_parser("set_today", help="Set the current date")
+    parser_set_today.add_argument("date", help="The date (YYYY-MM-DD) to set as today")
+
+    parser_advance_time = subparsers.add_parser("advance_time", help="Advance the current date")
+    parser_advance_time.add_argument("days", type=int, help="The number of days to advance")
+
+    parser_buy = subparsers.add_parser("buy", help="Buy a product")
+    parser_buy.add_argument("--product-name", required=True, help="Name of the product")
+    parser_buy.add_argument("--price", type=float, required=True, help="Price of the product")
+    parser_buy.add_argument("--expiration-date", required=True, help="Expiration date of the product (YYYY-MM-DD)")
+
+    parser_sell = subparsers.add_parser("sell", help="Sell a product")
+    parser_sell.add_argument("--product-name", required=True, help="Name of the product")
+    parser_sell.add_argument("--price", type=float, required=True, help="Selling price of the product")
+
+    parser_report = subparsers.add_parser("report", help="Generate reports")
+    subparsers_report = parser_report.add_subparsers(dest="report_type", title="report types")
+
+    parser_report_inventory = subparsers_report.add_parser("inventory", help="Generate inventory report")
+    parser_report_inventory.add_argument("--now", action="store_true", help="Generate report for today")
+    parser_report_inventory.add_argument("--yesterday", action="store_true", help="Generate report for yesterday")
+
+    parser_report_revenue = subparsers_report.add_parser("revenue", help="Generate revenue report")
+    group_revenue = parser_report_revenue.add_mutually_exclusive_group(required=True)
+    group_revenue.add_argument("--today", action="store_true", help="Generate report for today")
+    group_revenue.add_argument("--date", help="Generate report for a specific date (YYYY-MM-DD)")
+
+    parser_report_profit = subparsers_report.add_parser("profit", help="Generate profit report")
+    group_profit = parser_report_profit.add_mutually_exclusive_group(required=True)
+    group_profit.add_argument("--today", action="store_true", help="Generate report for today")
+    group_profit.add_argument("--date", help="Generate report for a specific date (YYYY-MM-DD)")
+
+    args = parser.parse_args()
+
+    create_data_dir()
+
+    if args.command == "set_today":
+        set_today(datetime.datetime.strptime(args.date, "%Y-%m-%d").date())
+    elif args.command == "advance_time":
+        advance_time(args.days)
+    elif args.command == "buy":
+        buy_product(args.product_name, args.price, args.expiration_date)
+    elif args.command == "sell":
+        sell_product(args.product_name, args.price)
+    elif args.command == "report":
+        if args.report_type == "inventory":
+            if args.now:
+                report_date = get_today()
+            elif args.yesterday:
+                report_date = get_today() - datetime.timedelta(days=1)
+            else:
+                parser_report_inventory.print_help()
+                return
+            inventory_report = get_inventory_report()
+            if inventory_report:
+                header = ["Product Name", "Count", "Buy Price", "Expiration Date"]
+                inventory_report.insert(0, header)
+                export_csv("inventory_report.csv", inventory_report)
+            else:
+                print("No products in inventory.")
+        elif args.report_type == "revenue":
+            if args.today:
+                report_date = get_today()
+            elif args.date:
+                report_date = datetime.datetime.strptime(args.date, "%Y-%m-%d").date()
+            else:
+                parser_report_revenue.print_help()
+                return
+            revenue = get_revenue_report(report_date)
+            print(f"Revenue for {report_date}: {revenue}")
+        elif args.report_type == "profit":
+            if args.today:
+                report_date = get_today()
+            elif args.date:
+                report_date = datetime.datetime.strptime(args.date, "%Y-%m-%d").date()
+            else:
+                parser_report_profit.print_help()
+                return
+            profit = get_profit_report(report_date)
+            print(f"Profit for {report_date}: {profit}")
 
 
 if __name__ == "__main__":
     main()
+
